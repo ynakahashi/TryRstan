86c6d56755dc6fdffe6f63f04165d782be135f39
ynakahashi <ynakahashi1003@gmail.com>
2017-03-30 21:58:26 +0900

    Start writing

---
+++ 08_01_TryHierarchicalBayes.R
@@ -0,0 +1,21 @@
+################################################################################
+##
+## Try Hierarchical Bayes Model by Rstan
+##
+## written by Y.Nakahashi 
+## 2017/3/30
+##
+################################################################################
+work_dir <- "/Users/nakahashi/Desktop/GitTest/TryRstan"
+setwd(work_dir)
+
+
+
+
+
+
+
+
+
+
+
