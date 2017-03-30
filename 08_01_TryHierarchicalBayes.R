62062069e1adb974f9acf125ee32bd2ffff3428d
ynakahashi <ynakahashi1003@gmail.com>
2017-03-30 22:40:31 +0900

    no message

--- 08_01_TryHierarchicalBayes.R
+++ 08_01_TryHierarchicalBayes.R
@@ -6,10 +6,77 @@
 ## 2017/3/30
 ##
 ################################################################################
+
+################################################################################
+## Environmental Settings
+################################################################################
+## set working directory
 work_dir <- "/Users/nakahashi/Desktop/GitTest/TryRstan"
 setwd(work_dir)
 
+## load rstan
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
 
+################################################################################
+## Run stan
+################################################################################
+## load sample data
+dat <- read.csv("data-sarary-2.txt", stringsAsFactors = F)
+
+## plot
+dat$KID <- as.factor(dat$KID)
+gp <- ggplot(dat, aes(X, Y, shape=KID))
+gp <- gp + theme_bw(base_size = 18)
+gp <- gp + geom_point(size = 2)
+plot(gp)   
+
+
+
+## prepare data
+dat_sar <- list(N = nrow(dat),
+                X = dat$X,
+                Y = dat$Y,
+                KID = dat$KID)
+
+## fitting 8-1
+fit_01 <- stan(file = 'model_8_1.stan', 
+               data = dat_sar, 
+               iter = 10000,
+               chains = 4,
+               seed = 1234)
+res_01 <- rstan::extract(fit_01)
+
+## fit lm for camparison
+fit_11 <- lm(Y~X, data=dat)
+
+## plot result
+print(fit_01)
+print(summary(fit_11))
+print(summary(fit_11)$sigma)
+stan_trace(fit_01)
+stan_hist(fit_01)
+stan_dens(fit_01, separate_chains = T)
+stan_ac(fit_01, separate_chains = T)
+
+
+
+## fitting 8-2
+fit_02 <- stan(file = 'model_8_2.stan', 
+               data = dat_sar, 
+               iter = 10000,
+               chains = 4,
+               seed = 1234)
+res_01 <- rstan::extract(fit_01)
+
+print(fit_02)
+print(summary(fit_11))
+print(summary(fit_11)$sigma)
+stan_trace(fit_02)
+stan_hist(fit_02)
+stan_dens(fit_02, separate_chains = T)
+stan_ac(fit_02, separate_chains = T)
 
 
 
