2940c22c068ee01f3712c7e71af0073dd947a90e
ynakahashi <ynakahashi1003@gmail.com>
2017-12-12 00:34:47 +0900

    no message

--- Script/Detect_Confounding_Factor.R
+++ Script/Detect_Confounding_Factor.R
@@ -24,6 +24,7 @@ options(mc.cores = parallel::detectCores())
 ################################################################################
 ## Simulate confounded data
 ################################################################################
+set.seed(123)
 N      <- 100
 x_real <- rnorm(N)
 x_spur <- rnorm(N, x_real)
@@ -55,33 +56,24 @@ fit_01 <- stan(file = './StanModel/Detect_Confound_Factor.stan',
                seed = 1234)
 
 
-
-
-
-## load sample data
-data(iris)
-dat <- iris
-
-## Create Y
-dat$Y   <- 0.8 * dat$Sepal.Length + 1.2 * dat$Petal.Length + rnorm(nrow(dat), 0, 1)
-dat_Ana <- dat[, -c(5)]
-
-## prepare data
-dat_Iris <- list(N  = nrow(dat_Ana),
-                 D  = 4,
-                 X = dat_Ana[, 1:4],
-                 Y = dat_Ana[, 5])
-
-
 ################################################################################
-## Fit via stan
+## Check result
 ################################################################################
-## fitting 
-fit_01 <- stan(file = './StanModel/Sparse_Modeling.stan', 
-               data = dat_Iris, 
-               iter = 10000,
-               chains = 4,
-               seed = 1234)
+summary(fit_01)$summary[, c("mean", "2.5%", "50%", "97.5%", "Rhat")]
+summary(lm(Y ~ Sepal.Length + Petal.Length, data = dat_Ana))
 
+## Parameters
+plot(fit_01)
 
+## Trace plot
+pars <- c("beta[1]", "beta[2]", "beta[3]", "beta[4]")
+stan_trace(fit_01, pars = pars)
 
+## Histogram
+stan_hist(fit_01, pars = pars)
+
+### Density
+stan_dens(fit_01, separate_chains = T, pars = pars)
+
+### Auto-correlation of the samples
+stan_ac(fit_01, separate_chains = T, pars = pars)
