8aec03f7da878f744b9f83c169d75c274e2ac3c4
ynakahashi <ynakahashi1003@gmail.com>
2017-12-11 23:30:55 +0900

    no message

--- Script/Detect_Confounding_Factor.R
+++ Script/Detect_Confounding_Factor.R
@@ -39,14 +39,49 @@ step(res.lm)
 
 
 ################################################################################
-## Estimate via stan
+## Fit via stan
 ################################################################################
+dat_stan <- list(
+   N = nrow(dat),
+   D = 2,
+   X = dat[, 2:3],
+   Y = dat[, 1]
+)
+
+fit_01 <- stan(file = './StanModel/Detect_Confound_Factor.stan', 
+               data = dat_stan, 
+               iter = 3000,
+               chains = 4,
+               seed = 1234)
 
 
 
 
 
+## load sample data
+data(iris)
+dat <- iris
 
+## Create Y
+dat$Y   <- 0.8 * dat$Sepal.Length + 1.2 * dat$Petal.Length + rnorm(nrow(dat), 0, 1)
+dat_Ana <- dat[, -c(5)]
+
+## prepare data
+dat_Iris <- list(N  = nrow(dat_Ana),
+                 D  = 4,
+                 X = dat_Ana[, 1:4],
+                 Y = dat_Ana[, 5])
+
+
+################################################################################
+## Fit via stan
+################################################################################
+## fitting 
+fit_01 <- stan(file = './StanModel/Sparse_Modeling.stan', 
+               data = dat_Iris, 
+               iter = 10000,
+               chains = 4,
+               seed = 1234)
 
 
 
